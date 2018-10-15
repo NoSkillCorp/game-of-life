3d8c1bac2d745cc35b73637b46036cb56bfd7bdb
thomas.comes <thomas.comes@matters.tech>
2018-10-15 13:51:09 +0200

    Added auto play

--- src/components/board.js
+++ src/components/board.js
@@ -1,126 +1,144 @@
-import React from 'react'
-import CellRow from './row'
-import Navigation from './navigation'
+import React from 'react';
+import CellRow from './row';
+import Navigation from './navigation';
 
 class Board extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      size: this.props.size ? parseInt(this.props.size) : 10,
-    }
+      play: false,
+      size: this.props.size ? parseInt(this.props.size, 10) : 10,
+    };
 
     this.state = {
       ...this.state,
-      grid: this.initGrid()
-    }
+      grid: this.initGrid(),
+    };
   }
 
   initGrid() {
-    return Array(this.state.size).fill(Array(this.state.size).fill(0))
+    return Array(this.state.size).fill(Array(this.state.size).fill(0));
   }
 
-  async handleClick(i,row) {
-      let newGrid = [...this.state.grid]
-      newGrid[i] = row
-      await this.setState({
-        grid: newGrid
-      })
+  async handleClick(i, row) {
+    const newGrid = [...this.state.grid];
+    newGrid[i] = row;
+    await this.setState({
+      grid: newGrid,
+    });
+  }
+
+  async run() {
+    this.handleNext();
+    if (this.state.play) {
+      setTimeout(this.run.bind(this), 10);
+    }
+  }
+
+  async play() {
+    await this.setState({ play: true });
+    this.run();
+  }
+
+  async stop() {
+    this.setState({ play: false });
   }
 
   async handleNext() {
-    let newGrid = [];
+    const newGrid = [];
     for (let i = 0; i < this.state.size; i++) {
-      let newColumn = []
+      const newColumn = [];
       for (let j = 0; j < this.state.size; j++) {
-        if (this.shouldILive(i,j)) {
-          newColumn.push(1)
+        if (this.shouldILive(i, j)) {
+          newColumn.push(1);
         } else {
-          newColumn.push(0)
+          newColumn.push(0);
         }
       }
-      newGrid.push(newColumn)
+      newGrid.push(newColumn);
     }
 
     await this.setState({
-      grid: newGrid
-    })
+      grid: newGrid,
+    });
   }
 
-  shouldILive(i,j) {
-    let neighbors = this.sumNeighbors(i,j)
+  shouldILive(i, j) {
+    const neighbors = this.sumNeighbors(i, j);
     if (neighbors === 3) {
-      return true
-    } else {
-      if (this.state.grid[i][j] === 1 && neighbors === 2) {
-        return true
-      }
+      return true;
     }
-    return false
+    if (this.state.grid[i][j] === 1 && neighbors === 2) {
+      return true;
+    }
+
+    return false;
   }
 
-  sumNeighbors(i,j) {
-    let sum = 0
+  sumNeighbors(i, j) {
+    let sum = 0;
     if (i !== 0 && j !== 0) {
-      sum += this.state.grid[i-1][j-1]
+      sum += this.state.grid[i - 1][j - 1];
     }
 
     if (i !== this.state.size - 1 && j !== this.props.size - 1) {
-      sum += this.state.grid[i+1][j+1]
+      sum += this.state.grid[i + 1][j + 1];
     }
 
     if (i !== 0 && j !== this.props.size - 1) {
-      sum += this.state.grid[i-1][j+1]
+      sum += this.state.grid[i - 1][j + 1];
     }
 
     if (i !== this.props.size - 1 && j !== 0) {
-      sum += this.state.grid[i+1][j-1]
+      sum += this.state.grid[i + 1][j - 1];
     }
 
     if (i !== 0) {
-      sum += this.state.grid[i-1][j]
+      sum += this.state.grid[i - 1][j];
     }
 
     if (j !== 0) {
-      sum += this.state.grid[i][j-1]
+      sum += this.state.grid[i][j - 1];
     }
 
     if (j !== this.props.size - 1) {
-      sum += this.state.grid[i][j+1]
+      sum += this.state.grid[i][j + 1];
     }
 
     if (i !== this.props.size - 1) {
-      sum += this.state.grid[i+1][j]
+      sum += this.state.grid[i + 1][j];
     }
 
-    return sum
+    return sum;
   }
 
   renderCells() {
-    let cellsGrid = []
-    for(let i = 0; i < this.state.size; i++) {
+    const cellsGrid = [];
+    for (let i = 0; i < this.state.size; i++) {
       cellsGrid.push(
         <CellRow
-          click={async (row) => await this.handleClick(i,row)}
-          subkey={'R' + i.toString()}
-          key={'R' + i.toString()}
+          click={async row => this.handleClick(i, row)}
+          subkey={`R${i.toString()}`}
+          key={`R${i.toString()}`}
           size={this.state.size}
           row={this.state.grid[i]}
-        />
-      )
+        />,
+      );
     }
-    return cellsGrid
+    return cellsGrid;
   }
 
   render() {
     return (
       <div>
         <div>
-          <Navigation next={async () => await this.handleNext()}/>
+          <Navigation
+            next={() => this.handleNext()}
+            play={() => this.play()}
+            stop={() => this.stop()}
+          />
         </div>
-        <div className="cellContainer">
-          {this.renderCells()}
-        </div>
+        <div className="cellContainer">{this.renderCells()}</div>
       </div>
     );
   }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import { Nav, NavItem, Glyphicon } from 'react-bootstrap'
+import React from 'react';
+import { Nav, NavItem, Glyphicon } from 'react-bootstrap';
 
 class navigation extends React.Component {
-
   handleClick(eventKey) {
-    console.log(eventKey)
+    console.log(eventKey);
   }
 
   render() {
     return (
-    <Nav bsStyle="pills">
-      <NavItem eventKey={1} onClick={() => this.handleClick(1)}><Glyphicon glyph="backward" /></NavItem>
-      <NavItem eventKey={2} onClick={() => this.handleClick(2)}><Glyphicon glyph="stop" /></NavItem>
-      <NavItem eventKey={3} onClick={() => this.handleClick(3)}><Glyphicon glyph="play" /></NavItem>
-      <NavItem eventKey={4} onClick={() => this.props.next()}><Glyphicon glyph="forward" /></NavItem>
-    </Nav>
-    )
+      <Nav bsStyle="pills">
+        <NavItem eventKey={1} onClick={() => this.handleClick(1)}>
+          <Glyphicon glyph="backward" />
+        </NavItem>
+        <NavItem eventKey={2} onClick={() => this.props.stop()}>
+          <Glyphicon glyph="stop" />
+        </NavItem>
+        <NavItem eventKey={3} onClick={() => this.props.play()}>
+          <Glyphicon glyph="play" />
+        </NavItem>
+        <NavItem eventKey={4} onClick={() => this.props.next()}>
+          <Glyphicon glyph="forward" />
+        </NavItem>
+      </Nav>
+    );
   }
 }
 
-export default navigation
+export default navigation;
