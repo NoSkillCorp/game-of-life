d2acd7b0da997f4a6a3ef879c438bc44e97c61bc
thomas.comes <thomas.comes@matters.tech>
2018-10-15 13:19:24 +0200

    enhanced eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import './index.css';
-import Layout from './components/layout'
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import Layout from "./components/layout";
 
-ReactDOM.render(
-  <Layout />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Layout />, document.getElementById("root"));
